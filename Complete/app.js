9e3a1f252c6a53a98818d8f78c07a5b980369924
Ruddy Douglass <rdouglass@uplandsoftware.com>
2021-07-03 08:17:21 -0400

    allow search term to be captured & passed to the node backend with the sso auth token

--- Complete/app.js
+++ Complete/app.js
@@ -5,7 +5,7 @@
  */
 
 if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config();
+    require('dotenv').config();
 }
 var createError = require('http-errors');
 var express = require('express');
@@ -31,72 +31,77 @@ app.use(cookieParser());
 
 /* Turn off caching when developing */
 if (process.env.NODE_ENV !== 'production') {
-  app.use(express.static(path.join(__dirname, 'public'),
-                        { etag: false }));
+    app.use(express.static(path.join(__dirname, 'public'), { etag: false }));
 
-  app.use(function (req, res, next) {
-    res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
-    res.header('Expires', '-1');
-    res.header('Pragma', 'no-cache');
-    next()
-  });
+    app.use(function (req, res, next) {
+        res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
+        res.header('Expires', '-1');
+        res.header('Pragma', 'no-cache');
+        next();
+    });
 } else {
-  // In production mode, let static files be cached.
-  app.use(express.static(path.join(__dirname, 'public')));
+    // In production mode, let static files be cached.
+    app.use(express.static(path.join(__dirname, 'public')));
 }
 
 app.use('/home/index', indexRouter);
 app.use('/auth', authRouter);
 
-app.get('/dialog.html', (async (req, res) => {
-  return res.sendfile('dialog.html');
-}));
-
-app.get('/getuserdata', async function(req, res, next) {
-  const graphToken = req.get('access_token');
-
-  // Minimize the data that must come from MS Graph by specifying only the property we need ("name")
-  // and only the top 10 folder or file names.
-  // Note that the last parameter, for queryParamsSegment, is hardcoded. If you reuse this code in
-  // a production add-in and any part of queryParamsSegment comes from user input, be sure that it is
-  // sanitized so that it cannot be used in a Response header injection attack. 
-  const graphData = await getGraphData(graphToken, "/me/drive/root/children", "?$select=name&$top=10");
-
-  // If Microsoft Graph returns an error, such as invalid or expired token,
-  // there will be a code property in the returned object set to a HTTP status (e.g. 401).
-  // Relay it to the client. It will caught in the fail callback of `makeGraphApiCall`.
-  if (graphData.code) {
-      next(createError(graphData.code, "Microsoft Graph error " + JSON.stringify(graphData)));
-  }
-  else 
-  {
-    // MS Graph data includes OData metadata and eTags that we don't need.
-    // Send only what is actually needed to the client: the item names.
-    const itemNames = [];
-    const oneDriveItems = graphData['value'];
-    for (let item of oneDriveItems){
-        itemNames.push(item['name']);
-    }
-
-    res.send(itemNames)
-  }
+app.get('/dialog.html', async (req, res) => {
+    return res.sendfile('dialog.html');
 });
 
+app.get('/getuserdata/:searchTerm', async function (req, res, next) {
+    const graphToken = req.get('access_token');
+
+    console.log(req.params.searchTerm);
+
+    const itemNames = [];
+    res.send(itemNames);
+
+    // Minimize the data that must come from MS Graph by specifying only the property we need ("name")
+    // and only the top 10 folder or file names.
+    // Note that the last parameter, for queryParamsSegment, is hardcoded. If you reuse this code in
+    // a production add-in and any part of queryParamsSegment comes from user input, be sure that it is
+    // sanitized so that it cannot be used in a Response header injection attack.
+    //const graphData = await getGraphData(graphToken, "/me/drive/root/children", "?$select=name&$top=10");
+
+    //const SPpathwSearch = "/groups/{af65c412-5d67-4611-abe8-ec0a68e8b7f3}/drive/root/search(q='Category')";
+
+    const SPpathwSearch = `/groups/{af65c412-5d67-4611-abe8-ec0a68e8b7f3}/drive/root/search(q=${req.params.searchTerm})`;
+    const graphData = await getGraphData(graphToken, SPpathwSearch, '?select=name,id,webUrl');
+
+    // If Microsoft Graph returns an error, such as invalid or expired token,
+    // there will be a code property in the returned object set to a HTTP status (e.g. 401).
+    // Relay it to the client. It will caught in the fail callback of `makeGraphApiCall`.
+    if (graphData.code) {
+        next(createError(graphData.code, 'Microsoft Graph error ' + JSON.stringify(graphData)));
+    } else {
+        // MS Graph data includes OData metadata and eTags that we don't need.
+        // Send only what is actually needed to the client: the item names.
+        const itemNames = [];
+        const oneDriveItems = graphData['value'];
+        for (let item of oneDriveItems) {
+            itemNames.push(item['name']); //-,item['id'],item['webUrl'],item['thumbnails']
+        }
+        res.send(itemNames);
+    }
+});
 
 // Catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+    next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
--- Complete/public/javascripts/ssoAuthES6.js
+++ Complete/public/javascripts/ssoAuthES6.js
@@ -1,93 +1,116 @@
 /*
- * Copyright (c) Microsoft. All rights reserved. Licensed under the MIT license. See full license in root of repo. 
+ * Copyright (c) Microsoft. All rights reserved. Licensed under the MIT license. See full license in root of repo.
  *
  * This file shows how to use the SSO API to get a bootstrap token.
  */
 
- // If the add-in is running in Internet Explorer, the code must add support 
- // for Promises.
+// If the add-in is running in Internet Explorer, the code must add support
+// for Promises.
 if (!window.Promise) {
     window.Promise = Office.Promise;
 }
 
-Office.onReady(function(info) {
-    $(function() {
-        $('#getGraphDataButton').on('click',getGraphData);
+Office.onReady(function (info) {
+    $(function () {
+        $('#getGraphDataButton').on('click', getGraphData);
+        $('#btnSearch').on('click', searchForData);
     });
 });
 
+async function searchForData() {
+    console.log('searchForData');
+
+    var jSearch = document.getElementById('tSearch').value;
+
+    await getGraphData(jSearch);
+}
+
 let retryGetAccessToken = 0;
 
+let searchTerm = '';
+
 async function getGraphData() {
+    console.log('getGraphData');
+
+    searchTerm = document.getElementById('tSearch').value;
+    console.log(searchTerm);
+
     try {
-        let bootstrapToken = await OfficeRuntime.auth.getAccessToken({ allowSignInPrompt: true, allowConsentPrompt: true, forMSGraphAccess: true }); 
+        let bootstrapToken = await OfficeRuntime.auth.getAccessToken({
+            allowSignInPrompt: true,
+            allowConsentPrompt: true,
+            forMSGraphAccess: true,
+        });
         let exchangeResponse = await getGraphToken(bootstrapToken);
         if (exchangeResponse.claims) {
-            // Microsoft Graph requires an additional form of authentication. Have the Office host 
-            // get a new token using the Claims string, which tells AAD to prompt the user for all 
+            // Microsoft Graph requires an additional form of authentication. Have the Office host
+            // get a new token using the Claims string, which tells AAD to prompt the user for all
             // required forms of authentication.
-            let mfaBootstrapToken = await  OfficeRuntime.auth.getAccessToken({ authChallenge: exchangeResponse.claims });
+            let mfaBootstrapToken = await OfficeRuntime.auth.getAccessToken({ authChallenge: exchangeResponse.claims });
             exchangeResponse = await getGraphToken(mfaBootstrapToken);
         }
-        
+
         if (exchangeResponse.error) {
             // AAD errors are returned to the client with HTTP code 200, so they do not trigger
             // the catch block below.
             handleAADErrors(exchangeResponse);
-        } 
-        else 
-        {
+        } else {
             // For debugging:
             // showMessage("ACCESS TOKEN: " + JSON.stringify(exchangeResponse.access_token));
 
             // makeGraphApiCall makes an AJAX call to the MS Graph endpoint. Errors are caught
             // in the .fail callback of that call, not in the catch block below.
-            makeGraphApiCall(exchangeResponse.access_token);
+            console.log('calling makeGraphAPICall', jSearch);
+            makeGraphApiCall(exchangeResponse.access_token, searchTerm);
         }
-    }
-    catch(exception) {
+    } catch (exception) {
         // The only exceptions caught here are exceptions in your code in the try block
         // and errors returned from the call of `getAccessToken` above.
-        if (exception.code) { 
+        if (exception.code) {
             handleClientSideErrors(exception);
+        } else {
+            showMessage('EXCEPTION: ' + JSON.stringify(exception));
         }
-        else {
-            showMessage("EXCEPTION: " + JSON.stringify(exception));
-        } 
     }
 }
 
 async function getGraphToken(bootstrapToken) {
-    let response = await $.ajax({type: "GET", 
-        url: "/auth",
-        headers: {"Authorization": "Bearer " + bootstrapToken }, 
-        cache: false
+    let response = await $.ajax({
+        type: 'GET',
+        url: '/auth',
+        headers: { Authorization: 'Bearer ' + bootstrapToken },
+        cache: false,
     });
     return response;
 }
 
 function handleClientSideErrors(error) {
     switch (error.code) {
-
         case 13001:
-            // No one is signed into Office. If the add-in cannot be effectively used when no one 
-            // is logged into Office, then the first call of getAccessToken should pass the 
+            // No one is signed into Office. If the add-in cannot be effectively used when no one
+            // is logged into Office, then the first call of getAccessToken should pass the
             // `allowSignInPrompt: true` option. Since this sample does that, you should not see
-            // this error. 
-            showMessage("No one is signed into Office. But you can use many of the add-ins functions anyway. If you want to log in, press the Get OneDrive File Names button again.");  
+            // this error.
+            showMessage(
+                'No one is signed into Office. But you can use many of the add-ins functions anyway. If you want to log in, press the Get OneDrive File Names button again.'
+            );
             break;
         case 13002:
             // The user aborted the consent prompt. If the add-in cannot be effectively used when consent
             // has not been granted, then the first call of getAccessToken should pass the `allowConsentPrompt: true` option.
-            showMessage("You can use many of the add-ins functions even though you have not granted consent. If you want to grant consent, press the Get OneDrive File Names button again."); 
+            showMessage(
+                'You can use many of the add-ins functions even though you have not granted consent. If you want to grant consent, press the Get OneDrive File Names button again.'
+            );
             break;
         case 13006:
             // Only seen in Office on the web.
-            showMessage("Office on the web is experiencing a problem. Please sign out of Office, close the browser, and then start again."); 
+            showMessage(
+                'Office on the web is experiencing a problem. Please sign out of Office, close the browser, and then start again.'
+            );
             break;
         case 13008:
             // Only seen in Office on the web.
-            showMessage("Office is still working on the last operation. When it completes, try this operation again."); 
+            showMessage('Office is still working on the last operation. When it completes, try this operation again.');
             break;
         case 13010:
             // Only seen in Office on the web.
@@ -105,20 +128,15 @@ function handleAADErrors(exchangeResponse) {
     // On rare occasions the bootstrap token is unexpired when Office validates it,
     // but expires by the time it is sent to AAD for exchange. AAD will respond
     // with "The provided value for the 'assertion' is not valid. The assertion has expired."
-    // Retry the call of getAccessToken (no more than once). This time Office will return a 
+    // Retry the call of getAccessToken (no more than once). This time Office will return a
     // new unexpired bootstrap token.
-    if ((exchangeResponse.error_description.indexOf("AADSTS500133") !== -1)
-        &&
-        (retryGetAccessToken <= 0)) 
-    {
+    if (exchangeResponse.error_description.indexOf('AADSTS500133') !== -1 && retryGetAccessToken <= 0) {
         retryGetAccessToken++;
         getGraphData();
-    }
-    else 
-    {
+    } else {
         // For all other AAD errors, fallback to non-SSO sign-in.
-        // For debugging: 
-        // showMessage("AAD ERROR: " + JSON.stringify(exchangeResponse));                   
+        // For debugging:
+        // showMessage("AAD ERROR: " + JSON.stringify(exchangeResponse));
         dialogFallback();
     }
 }
